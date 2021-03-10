c08401878842372b64b33f667599a3199732b8f9
Nick Sumner <wsumner@sfu.ca>
2021-03-10 14:18:28 -0800

    Update for LLVM 11

--- lib/tolerator-inst/Tolerator.h
+++ lib/tolerator-inst/Tolerator.h
@@ -4,7 +4,7 @@
 
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/DenseSet.h"
-#include "llvm/IR/CallSite.h"
+#include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
